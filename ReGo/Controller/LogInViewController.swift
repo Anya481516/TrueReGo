d17762293ae8e8f6cc7f564eb09834e31843dfe7
Anya <melxxova@gmail.com>
2020-03-31 16:08:35 +0500

    login and registration page design done

---
+++ ReGo/Controller/LogInViewController.swift
@@ -0,0 +1,33 @@
+//
+//  LogInViewController.swift
+//  ReGo
+//
+//  Created by Анна Мельхова on 31.03.2020.
+//  Copyright © 2020 Anna Melkhova. All rights reserved.
+//
+
+import UIKit
+
+class LogInViewController : UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func editingStarted(_ sender: UITextField) {
+        // тут надо поднять вьюху йоу и так же еще сделать так чтобы когда закончили, она назад опускалась ух
+    }
+    
+    @IBAction func logInButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func signUpButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+}
